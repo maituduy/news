45358a8db531fd9a9301ed87a41863b89910a971
maituduy <maituduy1998@gmail.com>
2018-08-27 13:24:27 +0700

    M faker and Provider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -18,9 +18,9 @@ class AppServiceProvider extends ServiceProvider
     {
         //
         Schema::defaultStringLength(191);
-        // if (! $this->app->runningInConsole()) {
-        //     View::share('categories', Category::all());
-        // }
+        if (! $this->app->runningInConsole()) {
+            View::share('categories', Category::all());
+        }
     }
 
     /**
